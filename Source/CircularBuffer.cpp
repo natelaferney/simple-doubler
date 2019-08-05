65513920899a1abb970eedd214280e6d43ae1bc8
Nate LaFerney <natelaferney@gmail.com>
2019-08-05 17:03:19 -0500

    added circular buffer code

---
+++ Source/CircularBuffer.cpp
@@ -0,0 +1,46 @@
+#include "CircularBuffer.h"
+
+CircularBuffer::CircularBuffer(int sizeOfBuffer)
+{
+	buffer.resize(sizeOfBuffer, 0.0f);
+	bufferSize = sizeOfBuffer;
+	writePosition = 0;
+	offset = 1;
+	readPosition = (writePosition - offset) % bufferSize;
+}
+
+CircularBuffer::~CircularBuffer()
+{
+}
+
+void CircularBuffer::setSampleRate(int newSampleRate)
+{
+	sampleRate = newSampleRate;
+}
+
+void CircularBuffer::setReadPositionFromMilliseconds(int milliseconds)
+{
+	offset = (int)(((milliseconds / 1000) * sampleRate) + .5);
+	readPosition = (writePosition - offset) % bufferSize;
+}
+
+float CircularBuffer::read()
+{
+	float ret = buffer[readPosition++];
+	readPosition = (readPosition >= bufferSize) ? 0 : readPosition;
+	return ret;
+}
+
+void CircularBuffer::write(float val)
+{
+	buffer[writePosition++] = val;
+	writePosition = (writePosition >= bufferSize) ? 0 : writePosition;
+}
+
+void CircularBuffer::reset()
+{
+	std::fill(buffer.begin(), buffer.end(), 0.0f);
+	writePosition = 0;
+	offset = 1;
+	readPosition = (writePosition - offset) % bufferSize;
+}
--- Source/CircularBuffer.h
+++ Source/CircularBuffer.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <vector>
+
+class CircularBuffer
+{
+public:
+	CircularBuffer(int sizeOfBuffer);
+	void setSampleRate(int newSampleRate);
+	void setReadPositionFromMilliseconds(int milliseconds);
+	float read();
+	void write(float val);
+	void reset();
+	~CircularBuffer();
+
+private:
+	int bufferSize;
+	int readPosition;
+	int writePosition;
+	int offset;
+	int sampleRate;
+	std::vector<float> buffer;
+};
